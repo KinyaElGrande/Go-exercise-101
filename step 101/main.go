611c38dc036066beddb3e0fbf5dead03447cc3e3
kinyaelgrande <kinya.francis.mumbi@gmail.com>
2020-07-15 21:22:23 +0300

    can move the player

--- step 101/main.go
+++ step 101/main.go
@@ -5,7 +5,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+
+	"github.com/danicat/simpleansi"
 )
+
+type sprite struct {
+	row int
+	col int
+}
+
+var player sprite
 
 var maze []string
 
@@ -22,13 +32,127 @@ func loadMaze(file string) error {
 		line := scanner.Text()
 		maze = append(maze, line)
 	}
+
+	//capturing players position
+	for row, line := range maze {
+		for col, char := range line {
+			switch char {
+			case 'P':
+				player = sprite{row, col}
+			}
+		}
+	}
 	return nil
 }
 
 func printScreen() {
+	simpleansi.ClearScreen()
 	for _, line := range maze {
-		fmt.Println(line)
+		for _, chr := range line {
+			switch chr {
+			case '#':
+				fmt.Printf("%c", chr)
+			default:
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
 	}
+
+	simpleansi.MoveCursor(player.row, player.col)
+	fmt.Print("P")
+
+	// Move cursor outside of maze drawing area
+	simpleansi.MoveCursor(len(maze)+1, 0)
+}
+
+//Enablong Cbreak terminal mode
+func initialize() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to activate cbreak terminal mode:", err)
+	}
+}
+
+//Restoring COoked terminal mode
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to restore cooked terminal mode:", err)
+	}
+}
+
+//Reading from Stdin
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	} else if cnt >= 3 {
+		if buffer[0] == 0x1b && buffer[1] == '[' {
+			switch buffer[2] {
+			case 'A':
+				return "UP", nil
+			case 'B':
+				return "DOWN", nil
+			case 'C':
+				return "RIGHT", nil
+			case 'D':
+				return "LEFT", nil
+			}
+		}
+	}
+
+	return "", nil
+}
+
+//movement function
+func makeMove(oldRow, oldCol int, dir string) (newRow, newCol int) {
+	newRow, newCol = oldRow, oldCol
+
+	switch dir {
+	case "UP":
+		newRow = newRow - 1
+		if newRow < 0 {
+			newRow = len(maze) - 1
+		}
+	case "DOWN":
+		newRow = newRow + 1
+		if newRow == len(maze) {
+			newRow = 0
+		}
+	case "RIGHT":
+		newCol = newCol + 1
+		if newCol == len(maze[0]) {
+			newCol = 0
+		}
+	case "LEFT":
+		newCol = newCol - 1
+		if newCol < 0 {
+			newCol = len(maze[0]) - 1
+		}
+	}
+
+	if maze[newRow][newCol] == '#' {
+		newRow = oldRow
+		newCol = oldCol
+	}
+
+	return
+}
+func movePlayer(dir string) {
+	player.row, player.col = makeMove(player.row, player.col, dir)
 }
 
 func main() {
@@ -38,11 +162,27 @@ func main() {
 		return
 	}
 
+	initialize()
+	defer cleanup()
+
 	//Game loop
 	for {
 		printScreen()
 
+		//process keyboard inputs
+		input, err := readInput()
+		if err != nil {
+			log.Print("error reading input:", err)
+			break
+		}
 
-		break
+		// process movement
+		movePlayer(input)
+
+		//permanent break
+		if input == "ESC" {
+			break
+		}
+
 	}
 }
