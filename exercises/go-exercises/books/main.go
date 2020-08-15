4742383d5bfe8ea280e2dd6be93f810cd24c94d5
kinyaelgrande <kinya.francis.mumbi@gmail.com>
2020-08-15 19:57:26 +0300

    removed punctuations from the words

--- exercises/go-exercises/books/main.go
+++ exercises/go-exercises/books/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"regexp"
+	"strings"
 
 	"./bookdetails"
 	"./booklist"
@@ -71,13 +74,25 @@ func tryBookFunctions() {
 func overlappingWords(author string) {
 
 	bookchannel := getBooks(author)
-	// wordschannel := getWords(bookchannel)
-	// for _, w := range wordschannel {
-	// }
+	wordschannel := bookFiles(bookchannel)
 
-	for b := range bookchannel {
-		fmt.Printf("%+v \n", b)
+	var commonWords map[string]int
+	for b := range wordschannel {
+		if commonWords == nil {
+			commonWords = b
+		} else {
+			// find words in common...
+			commonWords = findCommon(commonWords, b)
+		}
+
+		// fmt.Printf("%+v \n", b)
 	}
+
+	fmt.Printf("Common words: %v\n", commonWords)
+}
+
+func findCommon(start map[string]int, newWords map[string]int) map[string]int {
+	return nil
 }
 
 //pipeline for getting books
@@ -99,27 +114,71 @@ func getBooks(author string) chan *booklist.Book {
 	return foundBooks
 }
 
-//book files pipeline
-func bookFiles(foundBooks chan *booklist.Book) chan string {
-	booksfile := make(chan string)
+/*
+book ---> "The cat in the hat came back. The cat ate food."
+our map...
+the: 2
+cat: 2
+in: 1
+came: 1
+back: 1
+ate: 1
+food: 1
+*/
+
+//bookFiles returns the words from the foundbooks
+func bookFiles(foundBooks chan *booklist.Book) chan map[string]int {
+	wordschan := make(chan map[string]int)
 	detailService := bookdetails.NewService(dataDirectory)
 
 	go func() {
 		for x := range foundBooks {
-			bookfile, _ := detailService.Get(x.Filename)
-
-			defer bookfile.Close()
-
-			start := make([]byte, 257)
-			count, readErr := bookfile.Read(start)
-			if readErr != nil {
-				log.Fatalf("Error reading book: %s", readErr)
-			}
-
-			fmt.Printf("\nFirst %d bytes of book: %s\n", count, start)
+			wordschan <- booktoMap(x, detailService)
 		}
-
+		close(wordschan)
 	}()
 
-	return booksfile
+	return wordschan
+}
+
+func booktoMap(book *booklist.Book, detailService *bookdetails.Service) map[string]int {
+	words := make(map[string]int)
+	bookfile, _ := detailService.Get(book.Filename)
+
+	defer bookfile.Close()
+
+	scanner := bufio.NewScanner(bookfile)
+	for scanner.Scan() {
+		addWords(scanner.Text(), words)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return words
+}
+
+func addWords(text string, wordMap map[string]int) {
+	// add all the words in `text` to `wordMap`
+	words := strings.Split(text, " ")
+
+	// remove punctuation
+	// lower-case
+	// 4 letters or longer
+
+	reg, err := regexp.Compile("[^a-zA-Z]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, w := range words {
+		processedWord := reg.ReplaceAllString(w, "")
+		value, found := wordMap[processedWord]
+		if found {
+			wordMap[processedWord] = value + 1
+		} else {
+			wordMap[processedWord] = 1
+		}
+	}
 }
